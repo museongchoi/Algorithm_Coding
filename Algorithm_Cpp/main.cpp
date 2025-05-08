bc61272682f88616ba5f39f719b6c65af55bebbe
museongchoi <antjd3303@hs.ac.kr>
2025-05-08 17:57:54 +0900

    [refactor] : 연결리스트 수정

--- Algorithm_Cpp/main.cpp
+++ Algorithm_Cpp/main.cpp
@@ -8,9 +8,41 @@ struct Node
 	Node(int v) : data(v), next(nullptr) {}
 };
 
-int main()
+// 리스트 앞에 삽입 (double_pointer 버전)
+void push_front(Node** head_ref, int value) 
+{
+	Node* newNode = new Node(value);
+
+	// 새 노드의 next 가 기존 head 를 가르키도록
+	newNode->next = *head_ref;
+
+	*head_ref = newNode;
+}
+
+void printList(Node* head)
+{
+	
+}
+
+void freeList(Node* head)
 {
 
+}
+
+int main()
+{
+	Node* head = nullptr;	// 빈 리스트
+
+	// push_front 호출 시 &head (Node**) 를 넘겨준다
+	push_front(&head, 10);
+	push_front(&head, 20);
+	push_front(&head, 30);
+	// 리스트 30 -> 20 -> 10
+
+	cout << "Linked List";
+	printList(head);	// 출력 : 30 20 10
+
+	freeList(head);
 
 	return 0;
 }
