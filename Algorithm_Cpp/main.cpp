da356297e9da061dfbaa5d957c3d6834f62259d5
museongchoi <antjd3303@hs.ac.kr>
2025-07-22 17:37:12 +0900

    [add] : LinkedList 구현 / 포인터

--- Algorithm_Cpp/main.cpp
+++ Algorithm_Cpp/main.cpp
@@ -1,93 +1,128 @@
 #include <iostream>
 using namespace std;
 
+typedef int element;
+
+// 노드 구조체 정의
 struct Node {
-	int data;	// 저장할 값
+	element data;	// 실제 데이터
 	Node* link;	// 다음 노드 포인터
-	Node(int v, Node* nxt = nullptr) 
-		: data(v), link(nxt) {}
 };
 
-/*  
-	노드 삽입 : head 포인터를 참조로 받아 수정
-	p == nullptr 이면 맨 앞, 그 외엔 p 다음에 삽입
-*/
-void InsertNode(Node*& head, Node* p, Node* newNode)
+// 새 노드 생성 : malloc -> new, free -> delete
+Node* create_node(element data, Node* link)
 {
-	if (head == nullptr)
+	Node* new_node = new Node;	// 힙에 Node 할당
+	new_node->data = data;		// 값 설정  
+	new_node->link = link;		// 다음 노드 연결
+	return new_node;			// 호출자에게 주소 반환
+}
+
+// phead : head 포인터의 주소, p : 삽입 위치 직전 노드, new_node : 삽입할 노드
+void insert_node(Node** phead, Node* p, Node* new_node)
+{
+	// (1) 리스트 비어 있을 때
+	if (*phead == nullptr)
 	{
-		// 빈 리스트 일때
-		head = newNode;
+		*phead = new_node;
+		new_node->link = nullptr;
 	}
-	else if (p == nullptr) {
-		// 맨 앞에 삽입
-		newNode->link = head;
-		head = newNode;
+	// (2) 맨 앞에 삽입하고 싶을 때 (p == nullptr)
+	else if (p == nullptr)
+	{
+		new_node->link = *phead; // *phead == head 가 가르키는 노드의 주소를 new_Node->link 가 가르키게 한다.
+		*phead = new_node; // head 가 가르키는 첫번째 노드를 new_node 로 변경.
+	}
+	// (3) 중간 또는 맨 끝 삽입(나머지 모든 경우)
+	else {
+		new_node->link = p->link;
+		p->link = new_node;
 	}
 }
 
-/* 노드 삭제: head 포인터를 참조로 받아 수정
-   p == nullptr 이면 맨 앞, 그 외엔 p 다음(tmp)을 삭제
-*/
-
-void removeNode(Node*& head, Node* p, Node* target)
+// phead : head 포인터의 주소, p : 삭제 위치 직전 노드, tmp : 삭제할 노드
+void remove_node(Node** phead, Node* p, Node* target)
 {
-	// 삭제할 노드가 없으면 무시
-	if (target == nullptr)
+	// phead 자체 null, 리스트가 비어 있거나, 삭제 대상이 널인 경우
+	if (phead == nullptr || *phead == nullptr || target == nullptr)
 	{
-		return;
+		return; // 삭제 불가
 	}
 
+	// 1) 맨 앞 노드를 삭제하려는 경우
 	if (p == nullptr)
 	{
 		// 리스트 맨 앞 삭제
-		head = target->link;
+		*phead = target->link;
+		delete target;
 	}
+	// 2) 중간 또는 마지막 삭제
 	else {
 		p->link = target->link;
+		delete target;
 	}
-	delete target;
 }
 
-// 리스트 출력
+// 리스트 순회하여 출력
 void display(Node* head)
 {
-	// 범위 : cur; 은 cur != nullptr 과 동일
-	for (Node* cur = head; cur; cur = cur->link)
+	Node* p = head;
+	while (p != nullptr)
 	{
-		cout << cur->data;
+		cout << p->data << " ";
+		p = p->link;
 	}
+
 	cout << "\n";
+
+	//// 범위 : cur; 은 cur != nullptr 과 동일
+	//for (Node* cur = head; cur; cur = cur->link)
+	//{
+	//	cout << cur->data;
+	//}
+	//cout << "\n";
 }
 
-/* 데이터로 노드 검색
-   찾으면 그 노드 포인터, 못 찾으면 nullptr 반환
-*/
-Node* searchNode(Node* head, int value)
+// 값이 i인 노드 검색 (못 찾으면 nullptr 반환)
+Node* search_node(Node* head, element value)
 {
-	for (Node* cur = head; cur; cur = cur->link)
+	Node* p = head;
+	while (p != nullptr)
 	{
-		if (cur->data == value)
+		if (value == p->data)
 		{
-			return cur;
+			return p;
 		}
+		p = p->link;
 	}
+
 	return nullptr;
+
+	//for (Node* cur = head; cur; cur = cur->link)
+	//{
+	//	if (cur->data == value)
+	//	{
+	//		return cur;
+	//	}
+	//}
+	//return nullptr;
 }
 
-/* 리스트 연결 : head1 끝에 head2 를 붙이고 head1 반환 */
+/* 리스트 연결 : head1 끝에 head2 시작을 붙이고 head1 반환 */
 Node* concat(Node* head1, Node* head2)
 {
-	if (!head1)
+	if (!head1) // if (head1 == nullptr) head1 이 비어 있으면
 	{
-		return head2;
+		return head2;	// head2 가 새 리스트
 	}
-	Node* cur = head1;
-	while (cur->link)
+
+	Node* p = head1;
+	while (p->link != nullptr) // 끝 노드를 찾을 때까지
 	{
-		cur = cur->link;
+		p = p->link;
 	}
-	cur->link = head2;
+	p->link = head2;	// 연결
+	
 	return head1;
 }
 
@@ -98,22 +133,23 @@ int main()
 	Node* list3 = nullptr;
 
 	// 1~3을 list1, 10~30을 list2 맨 앞 삽입
-	for (int i = 0; i <= 3; i++)
+	for (int i = 1; i <= 3; i++)
 	{
-		InsertNode(list1, nullptr, new Node(i));
-		InsertNode(list2, nullptr, new Node(i * 10));
+		insert_node(&list1, nullptr, create_node(i, nullptr));
+		insert_node(&list2, nullptr, create_node(i * 10, nullptr));
 	}
 
 	// list1에서 데이터 1 뒤에 4 삽입
-	InsertNode(list1, searchNode(list1, 1), new Node(4));
+	insert_node(&list1, search_node(list1, 1), create_node(4, nullptr));
 
 	display(list1); // 3, 2, 1, 4
 	display(list2); // 30, 20, 10
 	cout << "\n";
 
-	// 리스트 삭제
-	removeNode(list1, nullptr, list1);	// 맨 앞 삭제 (3)
-	removeNode(list2, searchNode(list2, 30), searchNode(list2, 20)); // 20 삭제
+	// list1 맨 앞 3 삭제
+	remove_node(&list1, nullptr, list1);
+	// list2에서 30 뒤의 20 삭제
+	remove_node(&list2, search_node(list2, 30), search_node(list2, 20));
 
 	display(list1);
 	display(list2);
--- Structure/LinkedList.cpp
+++ Structure/LinkedList.cpp
@@ -1,50 +1,182 @@
 #include <iostream>
 using namespace std;
 
-struct Node
-{
-	int data;
-	Node* next;
-	Node(int v) : data(v), next(nullptr) {}
+typedef int element;
+
+// 노드 구조체 정의
+struct Node {
+	element data;	// 실제 데이터
+	Node* link;	// 다음 노드 포인터
 };
 
-// 리스트 앞에 삽입 (double_pointer 버전)
-void push_front(Node** head_ref, int value)
+// 새 노드 생성 : malloc -> new, free -> delete
+Node* create_node(element data, Node* link)
 {
-	// 새 노드 생성
-	Node* newNode = new Node(value);
-
-	// 새 노드의 next 가 기존 head 를 가르키도록
-	newNode->next = *head_ref;
-
-	// head 를 새 노드로 업데이트
-	*head_ref = newNode;
+	Node* new_node = new Node;	// 힙에 Node 할당
+	new_node->data = data;		// 값 설정  
+	new_node->link = link;		// 다음 노드 연결
+	return new_node;			// 호출자에게 주소 반환
 }
 
-void printList(Node* head)
+// phead : head 포인터의 주소, p : 삽입 위치 직전 노드, new_node : 삽입할 노드
+void insert_node(Node** phead, Node* p, Node* new_node)
 {
-
+	// (1) 리스트 비어 있을 때
+	if (*phead == nullptr)
+	{
+		*phead = new_node;
+		new_node->link = nullptr;
+	}
+	// (2) 맨 앞에 삽입하고 싶을 때 (p == nullptr)
+	else if (p == nullptr)
+	{
+		new_node->link = *phead; // *phead == head 가 가르키는 노드의 주소를 new_Node->link 가 가르키게 한다.
+		*phead = new_node; // head 가 가르키는 첫번째 노드를 new_node 로 변경.
+	}
+	// (3) 중간 또는 맨 끝 삽입(나머지 모든 경우)
+	else {
+		new_node->link = p->link;
+		p->link = new_node;
+	}
 }
 
-void freeList(Node* head)
+// phead : head 포인터의 주소, p : 삭제 위치 직전 노드, tmp : 삭제할 노드
+void remove_node(Node** phead, Node* p, Node* target)
 {
+	// phead 자체 null, 리스트가 비어 있거나, 삭제 대상이 널인 경우
+	if (phead == nullptr || *phead == nullptr || target == nullptr)
+	{
+		return; // 삭제 불가
+	}
 
+	// 1) 맨 앞 노드를 삭제하려는 경우
+	if (p == nullptr)
+	{
+		// 리스트 맨 앞 삭제
+		*phead = target->link;
+		delete target;
+	}
+	// 2) 중간 또는 마지막 삭제
+	else {
+		p->link = target->link;
+		delete target;
+	}
+}
+
+// 리스트 순회하여 출력
+void display(Node* head)
+{
+	Node* p = head;
+	while (p != nullptr)
+	{
+		cout << p->data << " ";
+		p = p->link;
+	}
+
+	cout << "\n";
+
+	//// 범위 : cur; 은 cur != nullptr 과 동일
+	//for (Node* cur = head; cur; cur = cur->link)
+	//{
+	//	cout << cur->data;
+	//}
+	//cout << "\n";
+}
+
+// 값이 i인 노드 검색 (못 찾으면 nullptr 반환)
+Node* search_node(Node* head, element value)
+{
+	Node* p = head;
+	while (p != nullptr)
+	{
+		if (value == p->data)
+		{
+			return p;
+		}
+		p = p->link;
+	}
+
+	return nullptr;
+
+	//for (Node* cur = head; cur; cur = cur->link)
+	//{
+	//	if (cur->data == value)
+	//	{
+	//		return cur;
+	//	}
+	//}
+	//return nullptr;
+}
+
+/* 리스트 연결 : head1 끝에 head2 시작을 붙이고 head1 반환 */
+Node* concat(Node* head1, Node* head2)
+{
+	if (!head1) // if (head1 == nullptr) head1 이 비어 있으면
+	{
+		return head2;	// head2 가 새 리스트
+	}
+
+	Node* p = head1;
+	while (p->link != nullptr) // 끝 노드를 찾을 때까지
+	{
+		p = p->link;
+	}
+	p->link = head2;	// 연결
+
+	return head1;
 }
 
 int main()
 {
-	Node* head = nullptr;	// 빈 리스트
+	Node* list1 = nullptr;
+	Node* list2 = nullptr;
+	Node* list3 = nullptr;
 
-	// push_front 호출 시 &head (Node**) 를 넘겨준다
-	push_front(&head, 10);
-	push_front(&head, 20);
-	push_front(&head, 30);
-	// 리스트 30 -> 20 -> 10
+	// 1~3을 list1, 10~30을 list2 맨 앞 삽입
+	for (int i = 1; i <= 3; i++)
+	{
+		insert_node(&list1, nullptr, create_node(i, nullptr));
+		insert_node(&list2, nullptr, create_node(i * 10, nullptr));
+	}
 
-	cout << "Linked List";
-	printList(head);	// 출력 : 30 20 10
+	// list1에서 데이터 1 뒤에 4 삽입
+	insert_node(&list1, search_node(list1, 1), create_node(4, nullptr));
 
-	freeList(head);
+	display(list1); // 3, 2, 1, 4
+	display(list2); // 30, 20, 10
+	cout << "\n";
+
+	// list1 맨 앞 3 삭제
+	remove_node(&list1, nullptr, list1);
+	// list2에서 30 뒤의 20 삭제
+	remove_node(&list2, search_node(list2, 30), search_node(list2, 20));
+
+	display(list1);
+	display(list2);
+	cout << "\n";
+
+	// 리스트 연결
+	list3 = concat(list1, list2);
+	display(list3);
+
+	// 전체 메모리 해제
+	while (list3)
+	{
+		Node* nxt = list3->link;
+		delete list3;
+		list3 = nxt;
+	}
 
 	return 0;
 }
+
+// 출력
+/*
+3 2 1 4 
+30 20 10 
+
+2 1 4 
+30 10 
+
+2 1 4 30 10 
+*/
