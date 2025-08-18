7bf0561274d72e7295eee6600e7db96aced1665c
museongchoi <antjd3303@hs.ac.kr>
2025-08-18 14:59:14 +0900

    [feat] | Shell Sort

--- Algorithm_Cpp/main.cpp
+++ Algorithm_Cpp/main.cpp
@@ -2,33 +2,34 @@
 #include <vector>
 using namespace std;
 
-void Display()
+void Display(vector<int>& vec)
 {
-
-}
-
-void insertionSort(vector<int>& vec)
-{
-	int key, keyidx;
-
 	for (int i = 0; i < vec.size(); ++i)
 	{
+		cout << vec[i] << " ";
+	}
+	cout << endl;
+}
+
+void insertionSort(vector<int>& vec, int gap)
+{
+	int key, j;
+
+	for (int i = gap; i < vec.size(); ++i)
+	{
+		// gap 은 간격 즉, 간격 양쪽에 있는 값을 비교하는 것.
 		key = vec[i];
-		keyidx = i;
+		j = i - gap;
 		
-		for (int j = i - 1; j >= 0; --i)
+		// gap 차이 만큼의 비교 대상 | 0 보다 크거나 같아야 한다 && key 값과 비교
+		while (j >= 0 && vec[j] > key)
 		{
-			if (key < vec[j])
-			{
-				vec[j+1] = vec[j];
-				keyidx = j;
-			}
-			else
-			{
-				break;
-			}
+			vec[j + gap] = vec[j];	// 더 큰 값들을 한 칸(=gap만큼) 뒤로 민다
+			j -= gap;
 		}
-		vec[keyidx] = key;
+		// while 문을 빠져 나온다면 j값은 0보다 작거나 or vec[j] 가 key 값 보다 작은 값 이므로 더이상 교체가 안되는 것.
+		// 그러므로 j idx 값을 다시 앞으로 밀어서 돌아온 뒤에 해당 위치에 보관되어 있던 key 값을 저장.
+		vec[j + gap] = key; 
 	}
 }
 
@@ -38,23 +39,21 @@ void Shell_Sort(vector<int>& vec)
 
 	for (gap = vec.size() / 2; gap > 0; gap /= 2)
 	{
-		if (gap / 2 == 0)
-		{
-			gap++;
-		}
 		// 삽입 정렬
-		insertionSort(vec);
+		insertionSort(vec, gap);
 	}
-
 }
 
 int main()
 {
 	vector<int> vec = { 10, 8, 6, 20, 4, 3, 22, 1, 0, 15, 16 };
 
-
-	Display();
+	cout << "be" << endl;
+	Display(vec);
 
 	Shell_Sort(vec);
+
+	cout << "af" << endl;
+	Display(vec);
 	return 0;
 }
