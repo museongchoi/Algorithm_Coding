6db5499145dc5ded491baff53acb0225632fc13b
museongchoi <antjd3303@hs.ac.kr>
2025-11-12 18:42:55 +0900

    [feat] BOJ 10808 알파벳 개수 | array, algorithm::count 풀이
    README 10808 알파벳 개수 추가

---
+++ BOJ/Array/10808.cpp
@@ -0,0 +1,49 @@
+﻿// 배열 사용
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	string st;
+	cin >> st;
+
+	int arr[26] = { 0, };
+
+	for (char ch : st)
+	{
+		arr[ch - 'a'] += 1;
+	}
+
+	for (int i : arr)
+	{
+		cout << i << " ";
+	}
+
+	return 0;
+}
+
+// algorithm::count 풀이
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	string st;
+	cin >> st;
+
+	for (int i = 'a'; i <= 'z'; ++i)
+	{
+		cout << count(st.begin(), st.end(), i) << " ";
+	}
+
+	return 0;
+}
