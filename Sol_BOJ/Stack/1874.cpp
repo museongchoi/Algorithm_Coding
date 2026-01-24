702db75a033228893cfe61635c0569187b5d98b4
museongchoi <antjd3303@hs.ac.kr>
2026-01-24 18:37:38 +0900

    [refactor] : 1874 스택 수열 리팩토링

--- Sol_BOJ/Stack/1874.cpp
+++ Sol_BOJ/Stack/1874.cpp
@@ -1,4 +1,54 @@
-﻿#include <iostream>
+﻿// ver.2
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	vector<char> result;
+	stack<int> st;
+	int val = 1;
+
+	for (int i = 0; i < n; ++i)
+	{
+		int compareVal;
+		cin >> compareVal;
+
+		while (val <= compareVal)
+		{
+			st.push(val);
+			val += 1;
+			result.push_back('+');
+		}
+
+		if (st.top() == compareVal)
+		{
+			st.pop();
+			result.push_back('-');
+		}
+		else
+		{
+			cout << "NO";
+			return 0;
+		}
+	}
+
+	for (int i = 0; i < result.size(); ++i)
+	{
+		cout << result[i] << "\n";
+	}
+
+	return 0;
+}
+
+
+// ver.1
+#include <iostream>
 #include <vector>
 #include <stack>
 
@@ -30,54 +80,6 @@ int main()
 		}
 
 		if (st.top() == vec[idx])
-		{
-			st.pop();
-			result.push_back('-');
-		}
-		else
-		{
-			cout << "NO";
-			return 0;
-		}
-	}
-
-	for (int i = 0; i < result.size(); ++i)
-	{
-		cout << result[i] << "\n";
-	}
-
-	return 0;
-}
-
-// refactor
-#include <iostream>
-#include <vector>
-#include <stack>
-
-using namespace std;
-
-int main()
-{
-	int n;
-	cin >> n;
-
-	vector<char> result;
-	stack<int> st;
-	int val = 1;
-
-	for (int i = 0; i < n; ++i)
-	{
-		int compareVal;
-		cin >> compareVal;
-
-		while (val <= compareVal)
-		{
-			st.push(val);
-			val += 1;
-			result.push_back('+');
-		}
-
-		if (st.top() == compareVal)
 		{
 			st.pop();
 			result.push_back('-');
