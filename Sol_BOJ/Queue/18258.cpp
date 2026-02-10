7a7bef606a2ec35f6445304a883c52690361883a
museongchoi <antjd3303@hs.ac.kr>
2026-02-10 18:06:16 +0900

    [feat] : BOJ 18258 큐 구현 ver2

--- Sol_BOJ/Queue/18258.cpp
+++ Sol_BOJ/Queue/18258.cpp
@@ -1,4 +1,227 @@
-﻿// queue 구현
+﻿// ver.02 : queue 구현
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Node {
+	int data;
+	Node* link;
+};
+
+class Que {
+	Node* first;
+	Node* end;
+	int cnt;
+
+public:
+	Que() : first(nullptr), end(nullptr), cnt(0) {}
+	~Que() {}
+
+public:
+	void push(int inData)
+	{
+		Node* newNode = new Node;
+		newNode->data = inData;
+		newNode->link = nullptr;
+
+		if (first == nullptr)
+		{
+			first = newNode;
+			end = newNode;
+		}
+		else
+		{
+			end->link = newNode;
+			end = newNode;
+		}
+		++cnt;
+	}
+
+	void pop()
+	{
+		if (first == nullptr)
+		{
+			cout << -1 << "\n";
+			return;
+		}
+
+		Node* tmpNode = first;
+		first = first->link;
+		cout << tmpNode->data << "\n";
+		delete tmpNode;
+
+		--cnt;
+		if (first == nullptr)
+		{
+			end = nullptr;
+		}
+	}
+
+	void size()
+	{
+		cout << cnt << "\n";
+	}
+
+	void empty()
+	{
+		if (first == nullptr)
+		{
+			cout << 1 << "\n";
+		}
+		else
+		{
+			cout << 0 << "\n";
+		}
+	}
+
+	void front()
+	{
+		if (first == nullptr)
+		{
+			cout << -1 << "\n";
+			return;
+		}
+
+		cout << first->data << "\n";
+	}
+
+	void back()
+	{
+		if (first == nullptr)
+		{
+			cout << -1 << "\n";
+			return;
+		}
+
+		cout << end->data << "\n";
+	}
+
+};
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	Que q;
+
+	for (int i = 0; i < n; ++i)
+	{
+		string st;
+		cin >> st;
+
+		if (st == "push")
+		{
+			int val;
+			cin >> val;
+			q.push(val);
+		}
+		else if (st == "pop")
+		{
+			q.pop();
+		}
+		else if (st == "size")
+		{
+			q.size();
+		}
+		else if (st == "empty")
+		{
+			q.empty();
+		}
+		else if (st == "front")
+		{
+			q.front();
+		}
+		else if (st == "back")
+		{
+			q.back();
+		}
+
+	}
+
+	return 0;
+}
+
+// include queue
+#include <iostream>
+#include <queue>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	queue<int> que;
+
+	for (int i = 0; i < n; ++i)
+	{
+		string cmd;
+		cin >> cmd;
+
+		if (cmd == "push")
+		{
+			int val;
+			cin >> val;
+			que.push(val);
+		}
+		else if (cmd == "pop")
+		{
+			if (que.empty())
+			{
+				cout << -1 << "\n";
+			}
+			else
+			{
+				cout << que.front() << "\n";
+				que.pop();
+			}
+		}
+		else if (cmd == "size")
+		{
+			cout << que.size() << "\n";
+		}
+		else if (cmd == "empty")
+		{
+			cout << que.empty() << "\n";
+		}
+		else if (cmd == "front")
+		{
+			if (!que.empty())
+			{
+				cout << que.front() << "\n";
+			}
+			else
+			{
+				cout << -1 << "\n";
+			}
+		}
+		else if (cmd == "back")
+		{
+			if (!que.empty())
+			{
+				cout << que.back() << "\n";
+			}
+			else
+			{
+				cout << -1 << "\n";
+			}
+		}
+	}
+
+	return 0;
+}
+
+// ver.01 : queue 구현
 #include <iostream>
 #include <string>
 
@@ -144,81 +367,6 @@ int main()
 		else if (cmd == "back")
 		{
 			que.back();
-		}
-	}
-
-	return 0;
-}
-
-// include queue
-#include <iostream>
-#include <queue>
-#include <string>
-
-using namespace std;
-
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
-
-	int n;
-	cin >> n;
-
-	queue<int> que;
-
-	for (int i = 0; i < n; ++i)
-	{
-		string cmd;
-		cin >> cmd;
-
-		if (cmd == "push")
-		{
-			int val;
-			cin >> val;
-			que.push(val);
-		}
-		else if (cmd == "pop")
-		{
-			if (que.empty())
-			{
-				cout << -1 << "\n";
-			}
-			else
-			{
-				cout << que.front() << "\n";
-				que.pop();
-			}
-		}
-		else if (cmd == "size")
-		{
-			cout << que.size() << "\n";
-		}
-		else if (cmd == "empty")
-		{
-			cout << que.empty() << "\n";
-		}
-		else if (cmd == "front")
-		{
-			if (!que.empty())
-			{
-				cout << que.front() << "\n";
-			}
-			else
-			{
-				cout << -1 << "\n";
-			}
-		}
-		else if (cmd == "back")
-		{
-			if (!que.empty())
-			{
-				cout << que.back() << "\n";
-			}
-			else
-			{
-				cout << -1 << "\n";
-			}
 		}
 	}
 
